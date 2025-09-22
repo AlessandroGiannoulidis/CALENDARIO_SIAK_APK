06dba13775034698d2fef69b1418ea6058bfd5fb
AlessandroGiannoulidis <alessandro.giannoulidis.siak@gmail.com>
2025-09-22 17:13:57 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,5 @@ implementation("org.mnode.ical4j:ical4j:4.0.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 }
