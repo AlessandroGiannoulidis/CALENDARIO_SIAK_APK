fac68e14e42e77eb71f8c9ec0fd3f3059f6e93ab
AlessandroGiannoulidis <alessandro.giannoulidis.siak@gmail.com>
2025-09-22 15:47:39 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
+implementation("org.mnode.ical4j:ical4j:4.0.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
