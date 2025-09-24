a703fc6285212a1854ada2bf3a04a2c92bc5fdb7
AlessandroGiannoulidis <alessandro.giannoulidis.siak@gmail.com>
2025-09-24 15:08:58 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.tuopacchetto"
-        minSdk = 26
+        minSdk = 26  // Cambiato da 23 a 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
